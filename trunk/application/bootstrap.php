b9984ba55cab5052fef89516cb3a8eccd6dc8cc0
tiagoafseixas@gmail.com <tiagoafseixas@gmail.com@35fbfdf6-8a29-cbee-eb84-c7773cf98e87>
2011-11-14 23:23:57 +0000

    Acrescentado AntiCSRF;Optimizado bootstrap;Funções de logging alteradas

--- trunk/application/bootstrap.php
+++ trunk/application/bootstrap.php
@@ -14,34 +14,38 @@
  */
 session_start();
 define('BASE_PATH', dirname(dirname(__FILE__)));
+define('START_TIME', microtime(true));
+
 // Most Common Includes Go Here
 require 'functions.inc';
-require '../library/Framework/Registry.inc';
 require '../library/Framework/ACL.inc';
 require '../library/Framework/ACL/Role.inc';
-require '../library/Framework/Request.inc';
-require '../library/Framework/Response.inc';
 require '../library/Framework/Controller.inc';
+require '../library/Framework/Form.inc';
+require '../library/Framework/LogFactory.inc';
 require '../library/Framework/Model.inc';
 require '../library/Framework/Navigation.inc';
+require '../library/Framework/Navigation/Item.inc';
+require '../library/Framework/Registry.inc';
+require '../library/Framework/Request.inc';
+require '../library/Framework/Response.inc';
 
-global $debug;
-$debug = false && canDebug();
-
-global $start_time;
-$start_time = microtime(true);
-
-write_info_msg("Bootstraping", "start");
+define('DEBUG_ENABLED', false && can_debug());
 
 require 'Bootstrap/autoload.inc';
 require 'Bootstrap/initconfig.inc';
+
+write_info_msg("Bootstraping", "start");
+
 require 'Bootstrap/buildacl.inc';
 require 'Bootstrap/route.inc';
 require 'Bootstrap/accesscheck.inc';
+
+require '../library/Smarty-3.1.4/libs/Smarty.class.php';
+require '../library/Smarty-3.1.4/libs/plugins/block.t.php';
+
 require 'Bootstrap/dispatch.inc';
 require 'Bootstrap/renderview.inc';
 
 write_info_msg("Bootstraping", "done");
-$mem = round((memory_get_usage(TRUE) / 1048576), 2);
-write_info_msg("Bootstraping", "used $mem MB");
 session_write_close();
